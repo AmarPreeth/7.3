1c4c97f7beed9e05b5be153144f822c128976c45
AmarPreeth <amarp7919@gmail.com>
2025-10-29 23:13:29 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './Chat';
+
+function App() {
+  return (
+    <div>
+      <h1>⚡ Real-Time Chat App (Socket.IO)</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
